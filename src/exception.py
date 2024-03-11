2627cdf6c05a1ed78b23580dc1bcb535cfcfe9e4
arunnandam <naarku30@gmail.com>
2024-03-11 15:18:47 -0500

    Notebooks for EDA and Model Training

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 # To handle all the exceptions.
 
 import sys
-import logging 
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     #return type of sys
@@ -27,9 +27,11 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
+
+# Test Code to test Exception.py  
 if __name__ == "__main__":
     try:
         a=1/0
     except Exception as e:
+        logging.info("Divide by Zero Error")
         raise CustomException(e,sys)
